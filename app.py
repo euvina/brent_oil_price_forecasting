24640d609802c0b2545ac4f116bb6777efcb55ca
euvina <87256060+euvina@users.noreply.github.com>
2024-05-19 13:23:04 -0300

    Update app.py

--- app.py
+++ app.py
@@ -14,17 +14,9 @@ from prophet.plot import plot_plotly
 
 from sklearn.model_selection import TimeSeriesSplit
 
-from statsmodels.tsa.stattools import adfuller
-
 import pickle
 
-from utils import (normality_test, 
-                   create_warning, 
-                   create_quote, 
-                   create_curiosity, 
-                   create_insight, 
-                   create_analysis, 
-                   insert_image)
+from utils import *
 
 # layout
 st.set_page_config(layout='centered', 
